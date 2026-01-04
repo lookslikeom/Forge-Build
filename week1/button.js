7205c0432be91fd1c4ae8bb42cb84a1af682e9a3
lookslikeom <om@Oms-MacBook-Air.local>
2026-01-04 15:34:43 +0530

    all

---
+++ week1/button.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("myButton");
+const para = document.getElementById("dom-task");
+
+button.addEventListener("click", function () {
+  para.innerHTML = "First web3 task";
+});
